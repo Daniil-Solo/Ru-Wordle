2e16e2f88446c218de67f855bbaa9cd327699b56
Daniil-Solo <daniil.solo1723@gmail.com>
2023-08-05 12:12:20 +0500

    add model and admin-tool for Game

--- backend/words/admin.py
+++ backend/words/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Word
+from .models import Word, Game
 
 
 @admin.register(Word)
@@ -8,3 +8,25 @@ class WordAdmin(admin.ModelAdmin):
     search_fields = ("name",)
     list_filter = ("difficulty",)
     ordering = ("name",)
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ("id", "status", "start_date", "word_name")
+    readonly_fields = ("start_date", "word_name", "word_difficulty")
+    search_fields = ("word__name",)
+    list_filter = ("status", "word__difficulty")
+    ordering = ("start_date",)
+
+    @admin.display(description="Слово")
+    def word_name(self, obj):
+        return obj.word.name
+
+    @admin.display(description="Сложность")
+    def word_difficulty(self, obj):
+        word_difficulty_name = obj.word.difficulty
+        word_difficulty_label = None
+        for (name, label) in Word.DifficultyLevel.choices:
+            if word_difficulty_name == name:
+                word_difficulty_label = label
+        return word_difficulty_label
--- backend/words/models.py
+++ backend/words/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 
 
@@ -14,8 +15,37 @@ class Word(models.Model):
     )
 
     def __str__(self):
-        return {self.name}
+        return self.name
 
     class Meta:
         verbose_name = "Слово"
         verbose_name_plural = "Слова"
+        ordering = ("name", )
+
+
+class Game(models.Model):
+    class Status(models.TextChoices):
+        IN_PROCESS = "ip", "В процессе"
+        TIME_OVER = "to", "Время закончилось"
+        ATTEMPT_ENDED = "ae", "Попытки закончились"
+        VICTORY = "vy", "Победа"
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    word = models.ForeignKey(
+        to=Word, on_delete=models.CASCADE, related_name="games",
+        verbose_name="Слово",
+        editable=False
+    )
+    start_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата начала игры")
+    status = models.CharField(
+        max_length=2, choices=Status.choices, default=Status.IN_PROCESS,
+        verbose_name="Статус"
+    )
+
+    def __str__(self):
+        return f"Игра {self.id}"
+
+    class Meta:
+        verbose_name = "Игра"
+        verbose_name_plural = "Игры"
+        ordering = ("start_date", )
