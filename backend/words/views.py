5c70cbc1df8f830ce9046071f6ecc33574ce6430
Daniil-Solo <daniil.solo1723@gmail.com>
2023-08-05 15:34:31 +0500

    add right status codes for views

--- backend/words/views.py
+++ backend/words/views.py
@@ -5,7 +5,7 @@ from backend.settings import MAX_GAME_TIME
 
 def start_new_game(request):
     game = GameService.create_new_game()
-    response = JsonResponse({"status": "ok", "message": "Игра создана"})
+    response = JsonResponse({"message": "Игра создана"}, status=201)
     response.set_cookie("game_id", game.id, max_age=MAX_GAME_TIME, httponly=True)
     return response
 
@@ -14,16 +14,15 @@ def check_word(request):
     try:
         word = request.GET["word"]
     except KeyError:
-        return JsonResponse({"status": "error", "message": "Не отправлено слово!"})
+        return JsonResponse({"message": "Не отправлено слово!"}, status=400)
     try:
         game_id = request.COOKIES["game_id"]
         success = GameService.check_word(game_id, word)
         if success:
             GameService.set_victory_status(game_id)
-            response = JsonResponse({"status": "ok", "message": "Победа!"})
+            response = JsonResponse({"message": "Победа!"})
         else:
-            response = JsonResponse({"status": "error", "message": "Задумано другое слово!"})
+            response = JsonResponse({"message": "Задумано другое слово!"})
         return response
     except KeyError:
-        return JsonResponse({"status": "error", "message": "Игры не существует или она уже закончилась!"})
-
+        return JsonResponse({"message": "Игры не существует или она уже закончилась!"}, status=400)
